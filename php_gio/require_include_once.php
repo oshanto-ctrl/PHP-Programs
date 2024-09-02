ffaa1d54be907cf0b65aa6a4a224b9487d241b3b
Rejoan Siddiky <siddikyrejoan@gmail.com>
2024-09-02 23:09:17 +0600

    Where what is require

--- php_gio/require_include_once.php
+++ php_gio/require_include_once.php
@@ -13,6 +13,8 @@
 		# include will result in a warning, continue the script running.
 		# require will result in an error and stop the script running.
 
+		Overall reason, use require_once for a file.
+
 	*/
 
 	require "include_file.php"; // require_once will only require 1 time
