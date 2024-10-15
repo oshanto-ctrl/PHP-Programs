1ac5eebb738a9eaa3703832200e76001bc6c3bcb
Rejoan Siddiky <siddikyrejoan@gmail.com>
2024-10-15 19:55:33 +0600

    Trying to learn about php errors

---
+++ php_gio/errors.php
@@ -0,0 +1,20 @@
+<?php 
+	
+	// Error Handling
+	
+	/* 
+		Error reporting level can be changed
+		to get error as required 
+
+	*/
+	
+	// Turn of the error reporting
+	
+	/*
+	error_reporting(0);
+
+	echo 1/0 . "\n";
+	*/
+	
+
+ ?>
